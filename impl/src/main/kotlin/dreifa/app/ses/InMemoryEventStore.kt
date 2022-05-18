fbed193901851ac66190ca1d83a254af59a66347
Ian Morgan <ian.j.morgan@gmail.com>
2022-05-18 12:30:10 +0100

    added OpenTelemetry support

--- impl/src/main/kotlin/dreifa/app/ses/InMemoryEventStore.kt
+++ impl/src/main/kotlin/dreifa/app/ses/InMemoryEventStore.kt
@@ -1,29 +1,49 @@
 package dreifa.app.ses
 
+import dreifa.app.opentelemetry.OpenTelemetryContext
+import dreifa.app.opentelemetry.OpenTelemetryProvider
+import dreifa.app.registry.Registry
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.api.trace.SpanKind
+import io.opentelemetry.api.trace.StatusCode
+import io.opentelemetry.api.trace.Tracer
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
 /**
  * The most simple EventStore - everything is in memory
  */
-class InMemoryEventStore(initialCapacity: Int = 10) : EventStore {
+class InMemoryEventStore(registry: Registry = Registry(), initialCapacity: Int = 10) : EventStore {
     private val events: MutableList<Event> = ArrayList(initialCapacity)
     private val eventIdLookup: MutableMap<EventId, Int> = HashMap(initialCapacity)
+    private val tracer = registry.getOrNull(Tracer::class.java)
+    private val provider = registry.getOrNull(OpenTelemetryProvider::class.java)
+
     override fun read(ctx: ClientContext, query: EventQuery): List<Event> {
-        val lastEventIndex = checkLastEventId(0, query)
-        if (lastEventIndex == events.size) return emptyList()
-        return this.events
-            .subList(lastEventIndex, events.size)
-            .filter { checkFilter(it, query) }
+        val trc = TelemetryRequestContext(ctx, "events-read")
+        return runWithTelemetry(trc) {
+
+            val lastEventIndex = checkLastEventId(0, query)
+            if (lastEventIndex == events.size) {
+                emptyList()
+            } else {
+                this.events
+                    .subList(lastEventIndex, events.size)
+                    .filter { checkFilter(it, query) }
+            }
+        }
     }
 
     override fun store(ctx: ClientContext, events: List<Event>): EventWriter {
-        synchronized(this) {
-            var index = this.events.size
-            events.forEach {
-                this.events.add(it)
-                eventIdLookup[it.id] = index
-                index++
+        val trc = TelemetryRequestContext(ctx, "events-store")
+        runWithTelemetry(trc) {
+            synchronized(this) {
+                var index = this.events.size
+                events.forEach {
+                    this.events.add(it)
+                    eventIdLookup[it.id] = index
+                    index++
+                }
             }
         }
         return this
@@ -70,4 +90,45 @@ class InMemoryEventStore(initialCapacity: Int = 10) : EventStore {
             }
         }
     }
+
+    data class TelemetryRequestContext(val ctx: ClientContext, val spanName: String)
+
+    data class TelemetryExecutionContext(val otc: OpenTelemetryContext)
+
+
+    fun <O> runWithTelemetry(
+        trc: TelemetryRequestContext, block: ((tec: TelemetryExecutionContext) -> O)
+    ): O {
+        return if (provider != null && tracer != null) {
+            val span = tracer.spanBuilder(trc.spanName).setSpanKind(SpanKind.SERVER).startSpan()
+
+            try {
+                val telemetryContext = OpenTelemetryContext.fromSpan(span)
+                val result = block.invoke(
+                    TelemetryExecutionContext(telemetryContext),
+                )  // what if the result is streaming ? are we closing the span too soon?
+                completeSpan(span)
+                result
+            } catch (ex: Exception) {
+                completeSpan(span, ex)
+                throw ex
+            }
+        } else {
+            block.invoke(TelemetryExecutionContext(OpenTelemetryContext.root()))
+        }
+    }
+
+
+    private fun completeSpan(span: Span) {
+        span.setStatus(StatusCode.OK)
+        span.end()
+    }
+
+    private fun completeSpan(span: Span, ex: Throwable) {
+        span.recordException(ex)
+        span.setStatus(StatusCode.ERROR, ex.message!!)
+        span.end()
+    }
+
+
 }
